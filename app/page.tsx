ba7f3df88d3bd15df73ce9f19f855af30b88c267
Sanket <sanket_shivam@mckinsey.com>
2024-10-08 11:34:27 +0530

    Modifying UI

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,17 @@ import AboutMe from "@/public/components/AboutMe/AboutMe";
 import Footer from "@/public/components/Footer/Footer";
 import Header from "@/public/components/Header/Header";
 import Image from "next/image";
+import anime from 'animejs';
+
 
 export default function Home() {
   return (
-    <div className="page-container">
-      <Header logo="checking" />
-      <main className="main">
+    <div>
+      {/* <Header logo="checking" /> */}
+      {/* <main className="main"> */}
         <AboutMe/>
-      </main>
-      <Footer />
+      {/* </main> */}
+      {/* <Footer /> */}
     </div>
   );
 }
--- public/components/AboutMe/AboutMe.tsx
+++ public/components/AboutMe/AboutMe.tsx
@@ -1,11 +1,34 @@
+'use client'
+
 import "./_AboutMe.css";
 import Image from "next/image";
+import anime from 'animejs';
+import { useEffect } from "react";
+import Link from "next/link";
+
 
 const AboutMe: React.FC = () => {
+  useEffect(() => {
+    anime({
+      targets: 'body', // target the body or any div
+      backgroundColor: [
+        '#e4eef0', 
+        '#89CFF0', 
+        '#B9D9EB', 
+        '#D0F0C0',
+        '#def4da'
+      ],
+      easing: 'linear',
+      duration: 10000, // total duration for the animation (5 seconds)
+      loop: true,     // makes the animation loop infinitely
+      direction: 'alternate' // alternate direction for smooth transitions
+    });
+  }, []);
+
   return (
-    <>
-      <div className="about-me">
-        <div className="flex flex-row profile-picture-placeholder">
+    <>  
+    <div className="about-me">
+        <div className="center-container">
           <Image
             className="profile-picture-container"
             src="/assets/sanket-profile2.png"
@@ -13,23 +36,35 @@ const AboutMe: React.FC = () => {
             height={250}
             alt="Sanket Shivam"
           />
+          <div>
+            <div className="headline-h1">Sanket Shivam</div>
+            <div className="subtitle-h1">🚀 SDE-2 at McKinsey & Company</div>
+            <div className="subtitle-h1">
+              📚 A passionate Engineer, likes reading science and geopolitics
+            </div>
+          </div>
           <div className="social-media-icons">
-            <Image
+          <Link href="https://www.linkedin.com/in/sanket-shivam-82b03a103/" target="_blank" rel="noopener noreferrer">
+          <Image
+              src
+      ="/assets/linkedInIcon.png"
+              width={50}
+              height={50}
+              alt="Sanket Shivam"
+            />
+          </Link>
+          <Link href="https://www.instagram.com/sanket.shivam?igsh=MWRxMWc5Y2hmeTE0dQ%3D%3D&utm_source=qr" target="_blank" rel="noopener noreferrer">
+          <Image
               src="/assets/instagramIcon.png"
-              width={100}
-              height={80}
+              width={50}
+              height={50}
               alt="Sanket Shivam"
             />
-            <Image
-              src="/assets/linkedInIcon.png"
-              width={100}
-              height={100}
-              alt="Sanket Shivam"
-            />
+            </Link>
           </div>
         </div>
       </div>
-      <div className="about-text">
+      {/* <div className="about-text">
         <div className="intro headline-h1">
           <p>hey there! I am Sanket Shivam</p>
         </div>
@@ -54,7 +89,7 @@ const AboutMe: React.FC = () => {
             collaborate and innovate together!
           </p>
         </div>
-      </div>
+      </div> */}
     </>
   );
 };
